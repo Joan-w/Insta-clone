36e93775ecf3447afa018b663b2aeb089fa95690
joan-w <wamuyujoan4@gmail.com>
2020-06-01 15:17:49 +0300

    Modifying the template names

--- insta/views.py
+++ insta/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 from django.views.generic import (
     ListView,
     CreateView,
@@ -8,7 +9,17 @@ from django.views.generic import (
 # Create your views here.
 
 class PostListView(ListView):
-    template_name = 'insta/post_list.html'
+    template_name = 'insta/home.html'
     queryset = Post.objects.all()
     context_object_name = 'posts'
 
+class PostCreateView(CreateView):
+    template_name = 'insta/create_post.html'
+    form_class = PostForm
+    queryset = Post.objects.all()
+    success_url = '/'
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        form.instance.author = self.request.user
+        return super().form_valid(form)
