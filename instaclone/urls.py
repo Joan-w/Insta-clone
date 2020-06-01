0ec653cb1c7c66163a83d6f5b59c8b58c786a330
joan-w <wamuyujoan4@gmail.com>
2020-06-01 22:51:16 +0300

    Modifying

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -24,8 +24,8 @@ from insta import views as user_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^register/', user_views.register, name='register'),
-    url(r'^login/', auth_views.LoginView.as_view(), name='login'),
-    url(r'^logout/', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     url(r'', include('insta.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
