df04a63d8f166bcac4b74214ca00b4fb16ab7b9d
joan-w <wamuyujoan4@gmail.com>
2020-06-01 21:46:29 +0300

    Adding login and logout urls

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
 from insta import views as user_views
@@ -23,6 +24,8 @@ from insta import views as user_views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^register/', user_views.register, name='register'),
+    url(r'^login/', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(), name='logout'),
     url(r'', include('insta.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
