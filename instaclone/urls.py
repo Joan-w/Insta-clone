52f6f89a7325892094118cd2f1b7533133149380
joan-w <wamuyujoan4@gmail.com>
2020-06-01 23:19:19 +0300

    Registering a profile url

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     url(r'^register/', user_views.register, name='register'),
     url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url(r'^profile/', user_views.profile, name='profile'),
     url(r'', include('insta.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
