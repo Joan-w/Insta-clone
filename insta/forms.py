7a7eb4c9287d5806ce41e9f5b754b29fc68b6d87
joan-w <wamuyujoan4@gmail.com>
2020-06-01 21:14:35 +0300

    Creating a RegisterUserForm

--- insta/forms.py
+++ insta/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from .models import Post
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
@@ -14,4 +16,16 @@ class PostForm(forms.ModelForm):
         fields = [
             'image',
             'caption'
+        ]
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2'
         ]
