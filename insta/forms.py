55447fb185b26982f1e2afe84c06eee4016ea0d7
joan-w <wamuyujoan4@gmail.com>
2020-06-03 00:21:16 +0300

    Adding a Profile Update Form

--- insta/forms.py
+++ insta/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Post
+from .models import Post, Profile
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Field
 
@@ -29,3 +29,18 @@ class UserRegisterForm(UserCreationForm):
             'password1',
             'password2'
         ]
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = [
+            'username',
+            'email',
+        ]
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
