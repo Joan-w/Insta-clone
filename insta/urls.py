270b8185459e149cfe3023525e4485375e76cd8f
joan-w <wamuyujoan4@gmail.com>
2020-06-03 00:25:58 +0300

    Adding update and delete urls

--- insta/urls.py
+++ insta/urls.py
@@ -3,13 +3,17 @@ from .views import (
     PostListView,
     PostCreateView,
     PostDetailView,
+    PostUpdateView,
+    PostDeleteView
 )
 
 app_name = 'insta'
 
 urlpatterns = [
     # Local : http://127.0.0.1:8000/
-    url(r'^$', PostListView.as_view(), name='post_list'),
-    url(r'^new/', PostCreateView.as_view(), name='post_new'),
-    url(r'^<int:id>', PostDetailView.as_view(), name='post_details'),
+    url(r'^$', PostListView.as_view(), name='post-list'),
+    url(r'^create/', PostCreateView.as_view(), name='post-create'),
+    url(r'^post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    url(r'^post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    url(r'^post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
 ]
