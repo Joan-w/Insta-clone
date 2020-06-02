e3540c2f01f701415617d5e438a09b400804971d
joan-w <wamuyujoan4@gmail.com>
2020-06-03 01:12:22 +0300

    Updated STATIC_ROOT

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -122,8 +122,6 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -131,11 +129,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'login' 
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
-
-# Configure Django App for Heroku
-django_heroku.settings(locals())
